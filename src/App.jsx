b6d3dda157d1fbbd8ef77d4e8561d23fef168e5c
Richard Aiello <richaiello17@gmail.com>
2022-10-10 12:50:35 -0400

    Tested fetching country data with swr and axios.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import useSWR from 'swr';
+import axios from 'axios';
+
 const App = () => {
-  return null;
+  const fetcher = url => axios.get(url).then(res => res.data);
+
+  const { data, error } = useSWR(
+    'https://restcountries.com/v3.1/all',
+    fetcher
+  );
+
+  if (error) return <div>Request Failed</div>;
+  if (!data) return <div>Loading...</div>;
+
+  data ? console.log(data) : console.log('No Data');
+
+  return (
+    <div>
+      <h1>Country Data:</h1>
+    </div>
+  );
 };
 
 export default App;
